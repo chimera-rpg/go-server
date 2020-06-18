031c16c255ebfd37191a6f8e7cf5882d7203e465
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-18 15:19:33 -0700

    Fix object insert logic; add xyz to Tile

--- world/Tile.go
+++ world/Tile.go
@@ -6,20 +6,27 @@ import (
 
 // Tile represents a location on the ground.
 type Tile struct {
+	y, x, z    int // Location of the tile.
 	objects    []ObjectI
 	brightness int
 }
 
 // insertObject inserts the provided Object at the given index.
 func (tile *Tile) insertObject(object ObjectI, index int) error {
+	if index == -1 {
+		index = len(tile.objects)
+	}
+	if index == -1 {
+		index = 0
+	}
 	if object.GetTile() != nil {
 		object.GetTile().removeObject(object)
 	}
-	if len(tile.objects) == index {
-		tile.objects = append(tile.objects, object)
-	}
-	tile.objects = append(tile.objects[:index+1], tile.objects[index:]...)
-	tile.objects[index] = object
+
+	tile.objects = append(tile.objects[:index], append([]ObjectI{object}, tile.objects[index:]...)...)
+
+	// Update object's tile reference.
+	object.SetTile(tile)
 
 	return nil
 }
