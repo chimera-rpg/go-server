92d0d7fbffdc70046d12fb4ab2f81d6caddad9aa
kts of kettek (yan) <kettek1@kettek.net>
2020-09-28 02:54:41 -0700

    Add comment

--- config/Config.go
+++ config/Config.go
@@ -1,9 +1,10 @@
 package config
 
+// Config provides a structure that contains configurable options for the game server.
 type Config struct {
-	Address    string `yaml:"address,omitempty"`
-	UseTLS     bool   `yaml:"useTLS,omitempty"`
-	TLSKey     string `yaml:"tlsKey,omitempty"`
-	TLSCert    string `yaml:"tlsCert,omitempty"`
-	Tickrate   int    `yaml:"tickrate,omitempty"`
+	Address  string `yaml:"address,omitempty"`
+	UseTLS   bool   `yaml:"useTLS,omitempty"`
+	TLSKey   string `yaml:"tlsKey,omitempty"`
+	TLSCert  string `yaml:"tlsCert,omitempty"`
+	Tickrate int    `yaml:"tickrate,omitempty"`
 }
