07fc55b256641126439479ee1e5408088d340221
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-10-06 02:53:27 -0700

    Add OwnerRepeatCommand

--- world/OwnerCommand.go
+++ world/OwnerCommand.go
@@ -6,11 +6,21 @@ type OwnerCommand interface {
 
 // OwnerMoveCommand represents an owner command to move an object towards a given direction.
 type OwnerMoveCommand struct {
-	Y, X, Z int
-	Running bool // Whether or not it is a running move.
+	Y, X, Z   int
+	Running   bool // Whether or not it is a running move.
+	Canceling bool // Whether or not it is a canceling move.
 }
 
 // OwnerStatusCommand represents an owner command to set the status for its target object.
 type OwnerStatusCommand struct {
 	Status StatusI
 }
+
+// OwnerRepeatCommand embeds repeating commands.
+type OwnerRepeatCommand struct {
+	Command OwnerCommand
+	Cancel  bool
+}
+
+// OwnerClearCommand clears the owner's commands.
+type OwnerClearCommand struct{}
