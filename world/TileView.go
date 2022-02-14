073034b4f27210d8d09be8d0a47a89c2e5b5464d
kts of kettek <kts@kettek.net>
2022-02-13 17:52:32 -0800

    Add known IDs to TileView

--- world/TileView.go
+++ world/TileView.go
@@ -2,7 +2,8 @@ package world
 
 // TileView represents an Owner's view of a particular tile.
 type TileView struct {
-	unset   bool
-	visible bool
-	modTime uint16 // corresponds to the modTime of whatever tile this is supposed to reference.
+	unset    bool
+	visible  bool
+	modTime  uint16   // corresponds to the modTime of whatever tile this is supposed to reference.
+	knownIDs []uint32 // List of IDs last known in this tile.
 }
