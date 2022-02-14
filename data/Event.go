674b1e4d58ffab960b050954b4ea787d1d3832d3
kts of kettek <kts@kettek.net>
2022-02-13 17:51:51 -0800

    Add spawn placement fields

--- data/Event.go
+++ data/Event.go
@@ -1,6 +1,11 @@
 package data
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+
+	cdata "github.com/chimera-rpg/go-common/data"
+)
 
 // SpawnEventType represents the spawn event rule to use.
 type SpawnEventType uint8
@@ -38,10 +43,65 @@ var SpawnEventTypeToString = map[SpawnEventType]string{
 	ExclusiveWeightedSpawn: "Exclusive Weighted",
 }
 
+// TimeRange just defines a range between two times.
+type IntRange struct {
+	Min int   `yaml:"Min,omitempty"`
+	Max int   `yaml:"Max,omitempty"`
+	Not []int `yaml:"Not,omitempty"`
+}
+
+// Random returns a count between min and max, inclusive.
+func (t IntRange) Random() int {
+	if t.Min == t.Max {
+		return t.Max
+	}
+	// Iffy code to reroll if Not has values.
+	if len(t.Not) > 0 {
+		var r int
+		for done := false; !done; {
+			r = rand.Intn(t.Max-t.Min+1) + t.Min
+			match := 0
+			for _, i := range t.Not {
+				if r == i {
+					match++
+				}
+			}
+			if match == 0 {
+				done = true
+			}
+		}
+		return r
+	}
+	return rand.Intn(t.Max-t.Min+1) + t.Min
+}
+
+type SpawnConditions struct {
+	Matter   *cdata.MatterType `yaml:"Matter,omitempty"`
+	Blocking *cdata.MatterType `yaml:"Blocking,omitempty"`
+}
+
+type SpawnPlacement struct {
+	Overlap bool            `yaml:"Overlap,omitempty"`
+	Surface SpawnConditions `yaml:"Surface,omitempty"`
+	Air     SpawnConditions `yaml:"Air,omitempty"`
+	X       IntRange        `yaml:"X,omitempty"`
+	Y       IntRange        `yaml:"Y,omitempty"`
+	Z       IntRange        `yaml:"Z,omitempty"`
+}
+
 // SpawnArchetype is a container for pairing an Archetype with a Chance value.
 type SpawnArchetype struct {
-	Chance    float32    `yaml:"Chance,omitempty"`
-	Archetype *Archetype `yaml:"Archetype,omitempty"`
+	Chance    float32        `yaml:"Chance,omitempty"`
+	Archetype *Archetype     `yaml:"Archetype,omitempty"`
+	Count     IntRange       `yaml:"Count,omitempty"`
+	Retry     int            `yaml:"Retry,omitempty"` // Retry count if spawning fails.
+	Placement SpawnPlacement `yaml:"Placement,omitempty"`
+}
+
+type XYZOffset struct {
+	X IntRange `yaml:"X,omitempty"`
+	Y IntRange `yaml:"Y,omitempty"`
+	Z IntRange `yaml:"Z,omitempty"`
 }
 
 // SpawnEventResponse is an event response that causes zero or more items to spawn.
