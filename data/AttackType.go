81f907967928f4e67d707adad6a894e7b301feef
kts of kettek <kts@kettek.net>
2022-04-06 06:44:22 -0700

    Separate AttackType into AttackStyle

--- data/AttackType.go
+++ data/AttackType.go
@@ -1,17 +1,9 @@
 package data
 
-// AttackType represents the % damage type that a weapon does or armor protects from.
-type AttackType uint32
+type AttackStyle uint32
 
 const (
-	// NoAttackType should not occur.
-	NoAttackType AttackType = 0
-	// Physical represents physical attacks.
-	Physical = 1 << iota
-	// Arcane represents arcane attacks.
-	Arcane
-	// Spirit represents spirit attacks.
-	Spirit
+	NoAttackStyle AttackStyle = iota
 	// Impact represents physical attack types of a bludgeoning nature.
 	Impact
 	// Pierce represents physical attack types of a sharp, thrusting nature.
@@ -34,12 +26,7 @@ const (
 	Harm
 )
 
-// StringToAttackTypeMap is the map of strings to attack types.
-var StringToAttackTypeMap = map[string]AttackType{
-	"Physical": Physical,
-	"Arcane":   Arcane,
-	"Spirit":   Spirit,
-
+var StringToAttackStyleMap = map[string]AttackStyle{
 	"Impact": Impact,
 	"Pierce": Pierce,
 	"Edged":  Edged,
@@ -54,12 +41,7 @@ var StringToAttackTypeMap = map[string]AttackType{
 	"Harm": Harm,
 }
 
-// AttackTypeToStringMap is a map of attack types to strings.
-var AttackTypeToStringMap = map[AttackType]string{
-	Physical: "Physical",
-	Arcane:   "Arcane",
-	Spirit:   "Spirit",
-
+var AttackStyleToStringMap = map[AttackStyle]string{
 	Impact: "Impact",
 	Pierce: "Pierce",
 	Edged:  "Edged",
@@ -74,19 +56,52 @@ var AttackTypeToStringMap = map[AttackType]string{
 	Harm: "Harm",
 }
 
+// AttackType represents the % damage type that a weapon does or armor protects from.
+type AttackType uint32
+
+const (
+	// NoAttackType should not occur.
+	NoAttackType AttackType = 0
+	// Physical represents physical attacks.
+	Physical = 1 << iota
+	// Arcane represents arcane attacks.
+	Arcane
+	// Spirit represents spirit attacks.
+	Spirit
+)
+
+// StringToAttackTypeMap is the map of strings to attack types.
+var StringToAttackTypeMap = map[string]AttackType{
+	"Physical": Physical,
+	"Arcane":   Arcane,
+	"Spirit":   Spirit,
+}
+
+// AttackTypeToStringMap is a map of attack types to strings.
+var AttackTypeToStringMap = map[AttackType]string{
+	Physical: "Physical",
+	Arcane:   "Arcane",
+	Spirit:   "Spirit",
+}
+
 // AttackTypes is a map of AttackTypes to floats.
-type AttackTypes map[AttackType]float64
+type AttackTypes map[AttackType]map[AttackStyle]float64
 
 // UnmarshalYAML unmarshals, converting attack type strings into AttackTypes.
 func (a *AttackTypes) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	*a = make(AttackTypes)
-	var value map[string]float64
+	var value map[string]map[string]float64
 	if err := unmarshal(&value); err != nil {
 		return err
 	}
 	for k, v := range value {
 		if ak, ok := StringToAttackTypeMap[k]; ok {
-			map[AttackType]float64(*a)[ak] = v / 100
+			(*a)[ak] = make(map[AttackStyle]float64)
+			for k2, v2 := range v {
+				if sk, ok := StringToAttackStyleMap[k2]; ok {
+					(*a)[ak][sk] = v2 / 100
+				}
+			}
 		}
 	}
 	return nil
@@ -94,10 +109,15 @@ func (a *AttackTypes) UnmarshalYAML(unmarshal func(interface{}) error) error {
 
 // MarshalYAML marshals, converting AttackTypes into strings.
 func (a AttackTypes) MarshalYAML() (interface{}, error) {
-	r := make(map[string]float64)
+	r := make(map[string]map[string]float64)
 	for k, v := range a {
 		if sk, ok := AttackTypeToStringMap[k]; ok {
-			r[sk] = v * 100
+			r[sk] = make(map[string]float64)
+			for k2, v2 := range v {
+				if sk2, ok := AttackStyleToStringMap[k2]; ok {
+					r[sk][sk2] = v2 * 100
+				}
+			}
 		}
 	}
 	return r, nil
@@ -109,21 +129,13 @@ func (a AttackTypes) Add(o AttackTypes) {
 		if _, exists := a[k]; !exists {
 			a[k] = v
 		} else {
-			a[k] += v
+			for k2, v2 := range v {
+				if _, exists := a[k][k2]; !exists {
+					a[k][k2] = v2
+				} else {
+					a[k][k2] += v2
+				}
+			}
 		}
 	}
 }
-
-/*
-Impact
-Pierce
-Edged
-Fire
-etc.
-
----
-Physical
-Arcane
-Spirit
-
-*/
