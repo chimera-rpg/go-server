e0abd05eba3388e7c047c2fcab6b3006a1380e36
kts of kettek <kts@kettek.net>
2022-04-06 16:32:01 -0700

    Adjust damage attribute unmarshal/marshal

--- data/Damage.go
+++ data/Damage.go
@@ -1,11 +1,11 @@
 package data
 
 type Damage struct {
-	Value          int
-	AttributeBonus map[AttackType]map[AttributeType]float64
+	Value          int                    `yaml:"Value,omitempty"`
+	AttributeBonus DamageAttributeBonuses `yaml:"AttributeBonus,omitempty"`
 }
 
-func (d *Damage) Add(other Damage) {
+func (d *Damage) Add(other *Damage) {
 	d.Value += other.Value
 	for k, v := range other.AttributeBonus {
 		if _, ok := d.AttributeBonus[k]; !ok {
@@ -20,4 +20,42 @@ func (d *Damage) Add(other Damage) {
 			}
 		}
 	}
+}
+
+type DamageAttributeBonuses map[AttackType]map[AttributeType]float64
+
+// UnmarshalYAML unmarshals, converting attack type strings into AttackTypes.
+func (d *DamageAttributeBonuses) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	*d = make(DamageAttributeBonuses)
+	var value map[string]map[string]float64
+	if err := unmarshal(&value); err != nil {
+		return err
+	}
+	for k, v := range value {
+		if ak, ok := StringToAttackTypeMap[k]; ok {
+			(*d)[ak] = make(map[AttributeType]float64)
+			for k2, v2 := range v {
+				if sk, ok := StringToAttributeTypeMap[k2]; ok {
+					(*d)[ak][sk] = v2
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// MarshalYAML marshals, converting AttackTypes into strings.
+func (d DamageAttributeBonuses) MarshalYAML() (interface{}, error) {
+	r := make(map[string]map[string]float64)
+	for k, v := range d {
+		if sk, ok := AttackTypeToStringMap[k]; ok {
+			r[sk] = make(map[string]float64)
+			for k2, v2 := range v {
+				if sk2, ok := AttributeTypeToStringMap[k2]; ok {
+					r[sk][sk2] = v2
+				}
+			}
+		}
+	}
+	return r, nil
 }
