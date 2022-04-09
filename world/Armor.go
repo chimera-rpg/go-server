ad48694dc62e042b054e1f53ed0a6ef16b900af1
kts of kettek <kts@kettek.net>
2022-04-09 08:16:58 -0700

    Add Merge, Clone, and String

--- world/Armor.go
+++ world/Armor.go
@@ -1,6 +1,11 @@
 package world
 
-import "github.com/chimera-rpg/go-server/data"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/chimera-rpg/go-server/data"
+)
 
 /*
 
@@ -11,6 +16,18 @@ type Armor struct {
 }
 
 type Armors []Armor
+
+func (as Armors) String() (s string) {
+	for _, a := range as {
+		s += data.AttackTypeToStringMap[a.ArmorType] + "("
+		var styleStrings []string
+		for k, st := range a.Styles {
+			styleStrings = append(styleStrings, fmt.Sprintf("%s: %1.f%%", data.AttackStyleToStringMap[k], st*100))
+		}
+		s += strings.Join(styleStrings, ",") + ") "
+	}
+	return
+}
 
 func (as *Armors) Reduce(ds *Damages) {
 	for _, d := range *ds {
@@ -36,4 +53,36 @@ func (as *Armors) Reduce(ds *Damages) {
 			}
 		}
 	}
+}
+
+// Merge merges 2 armors, additively.
+func (as *Armors) Merge(as2 Armors) {
+	for _, a2 := range as2 {
+		for _, a := range *as {
+			if a.ArmorType == a2.ArmorType {
+				for armorStyle2, armorValue2 := range a2.Styles {
+					if _, ok := a.Styles[armorStyle2]; ok {
+						a.Styles[armorStyle2] += armorValue2
+					} else {
+						a.Styles[armorStyle2] = armorValue2
+					}
+				}
+			}
+		}
+	}
+}
+
+func (as Armors) Clone() (as2 Armors) {
+	for _, a := range as {
+		a2 := Armor{
+			ArmorType: a.ArmorType,
+			Styles:    make(map[data.AttackStyle]float64),
+		}
+		for k, v := range a.Styles {
+			a2.Styles[k] = v
+		}
+		as2 = append(as2, a2)
+	}
+
+	return as2
 }
