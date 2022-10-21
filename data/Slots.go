90ac36146f32383486c52d1f6a7b7b2b7b98fa45
kts of kettek <kts@kettek.net>
2022-10-20 18:35:01 -0700

    Add some comments

--- data/Slots.go
+++ data/Slots.go
@@ -1,12 +1,6 @@
 package data
 
-type SlotsNeeds struct {
-	Min    []string   `json:"Min" yaml:"Min,omitempty"`
-	MinIDs []StringID `yaml:"-"`
-	Max    []string   `json:"Max" yaml:"Max,omitempty"`
-	MaxIDs []StringID `yaml:"-"`
-}
-
+// Slots are used to define limits for equipment and characters.
 type Slots struct {
 	Has      []string   `json:"Has" yaml:"Has,omitempty"`
 	HasIDs   []StringID `yaml:"-"`
@@ -15,4 +9,12 @@ type Slots struct {
 	UsesIDs  []StringID `yaml:"-"`
 	Gives    []string   `json:"Gives" yaml:"Gives,omitempty"`
 	GivesIDs []StringID `yaml:"-"`
+}
+
+// SlotsNeeds are minimum and maximum slot requirements. These do not modify Has.
+type SlotsNeeds struct {
+	Min    []string   `json:"Min" yaml:"Min,omitempty"`
+	MinIDs []StringID `yaml:"-"`
+	Max    []string   `json:"Max" yaml:"Max,omitempty"`
+	MaxIDs []StringID `yaml:"-"`
 }
