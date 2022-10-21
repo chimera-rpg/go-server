df25210f6f2e6ea97eb7b36a77d71b76a743f8e6
kts of kettek <kts@kettek.net>
2022-10-20 20:32:56 -0700

    Add ObjectEquippable

---
+++ world/ObjectEquippable.go
@@ -0,0 +1,186 @@
+package world
+
+import (
+	"math"
+
+	cdata "github.com/chimera-rpg/go-common/data"
+	"github.com/chimera-rpg/go-server/data"
+)
+
+type ObjectEquippable struct {
+	Object
+	armors         Armors
+	finalArmors    Armors
+	changedArmors  bool
+	damages        Damages
+	finalDamages   Damages
+	changedDamages bool
+}
+
+func NewObjectEquippable(a *data.Archetype) (o *ObjectEquippable) {
+	o = &ObjectEquippable{
+		Object:         Object{Archetype: a},
+		changedArmors:  true,
+		changedDamages: true,
+	}
+
+	return
+}
+
+func (o *ObjectEquippable) Slots() []data.StringID {
+	return o.Archetype.Slots.HasIDs
+}
+
+// CalculateDamages calculates the weapon's base damages.
+func (o *ObjectEquippable) CalculateDamages() {
+	o.damages = Damages{}
+	o.finalDamages = Damages{}
+	if o.Archetype.Damage == nil || o.Archetype.Damage.Value == 0 {
+		return
+	}
+	for k, a := range o.Archetype.AttackTypes {
+		damage := Damage{
+			AttackType: k,
+			Styles:     make(map[cdata.AttackStyle]float64),
+			BaseDamage: float64(o.Archetype.Damage.Value),
+		}
+
+		// Calculate attack style damage.
+		for k2, d := range a {
+			damage.Styles[k2] = d
+			damage.StyleTotal += damage.Styles[k2]
+		}
+
+		o.damages = append(o.damages, damage)
+	}
+}
+
+// GetDamages returns the adjusted Damages for the given character's skills, competencies, and attributes.
+func (o *ObjectEquippable) GetDamages(c *ObjectCharacter) (damages Damages, err error) {
+	if !o.changedDamages {
+		return o.finalDamages, nil
+	}
+	// Multiply by the weapon's skills
+	totalSkill := 0.0
+	totalSkillCount := 0
+	for _, s := range o.Archetype.SkillTypes {
+		v, ok := c.Archetype.Skills[s]
+		if !ok {
+			// No skill, we cannot process!
+			return nil, &MissingSkillError{s}
+		}
+		totalSkill += math.Floor(v.Experience)
+		totalSkillCount++
+	}
+	totalSkill /= float64(totalSkillCount)
+
+	// Get our competency float modifier.
+	totalCompetency := 0.0
+	totalCompetencyCount := 0
+	for _, ct := range o.Archetype.CompetencyTypes {
+		v, ok := c.Archetype.Competencies[ct]
+		if !ok {
+			// No competency, we cannot process!
+			return nil, &MissingCompetencyError{ct}
+		}
+		totalCompetency += v.Efficiency
+		totalCompetencyCount++
+	}
+	totalCompetency /= float64(totalCompetencyCount)
+	totalCompetency = 0.5 + totalCompetency/2
+
+	for _, damage := range o.damages {
+		// Calculate bonus damage.
+		if bonus, ok := o.Archetype.Damage.AttributeBonus[damage.AttackType]; ok {
+			for attrK, attrV := range bonus {
+				if damage.AttackType == cdata.Physical {
+					charAttr := c.Archetype.Attributes.Physical.GetAttribute(attrK)
+					damage.AttributeDamage += float64(charAttr) * attrV
+				} else if damage.AttackType == cdata.Arcane {
+					charAttr := c.Archetype.Attributes.Arcane.GetAttribute(attrK)
+					damage.AttributeDamage += float64(charAttr) * attrV
+				} else if damage.AttackType == cdata.Spirit {
+					charAttr := c.Archetype.Attributes.Spirit.GetAttribute(attrK)
+					damage.AttributeDamage += float64(charAttr) * attrV
+				}
+			}
+		}
+		// Calculate attack style damage.
+		for k, _ := range damage.Styles {
+			damage.Styles[k] *= (totalSkill * totalCompetency)
+			damage.StyleTotal += damage.Styles[k]
+		}
+		damages = append(damages, damage)
+	}
+	o.finalDamages = damages
+	o.changedDamages = false
+	return damages, nil
+}
+
+// CalculateArmors calculates the object's base armor values.
+func (o *ObjectEquippable) CalculateArmors() {
+	o.armors = Armors{}
+	o.finalArmors = Armors{}
+	if o.Archetype.Armor == 0 {
+		return
+	}
+	var armors Armors
+	for k, a := range o.Archetype.Resistances {
+		armor := Armor{
+			ArmorType: k,
+			Styles:    make(map[cdata.AttackStyle]float64),
+		}
+
+		for k2, s := range a {
+			armor.Styles[k2] = o.Archetype.Armor * s
+		}
+		armors = append(armors, armor)
+	}
+
+	o.armors = armors
+}
+
+// GetArmors returns the final calculated amount of armor using the given character's skills and competencies.
+func (o *ObjectEquippable) GetArmors(c *ObjectCharacter) (armors Armors, err error) {
+	if !o.changedArmors {
+		return o.finalArmors, nil
+	}
+	// Multiply by the armors's skills
+	totalSkill := 0.0
+	totalSkillCount := 0
+	for _, s := range o.Archetype.SkillTypes {
+		v, ok := c.Archetype.Skills[s]
+		if !ok {
+			// No skill, we cannot process!
+			return nil, &MissingSkillError{s}
+		}
+		totalSkill += math.Floor(v.Experience)
+		totalSkillCount++
+	}
+	totalSkill /= float64(totalSkillCount)
+
+	// Get our competency float modifier.
+	totalCompetency := 0.0
+	totalCompetencyCount := 0
+	for _, ct := range o.Archetype.CompetencyTypes {
+		v, ok := c.Archetype.Competencies[ct]
+		if !ok {
+			// No competency, we cannot process!
+			return nil, &MissingCompetencyError{ct}
+		}
+		totalCompetency += v.Efficiency
+		totalCompetencyCount++
+	}
+	totalCompetency /= float64(totalCompetencyCount)
+	totalCompetency = 0.5 + totalCompetency/2
+
+	for _, a := range o.armors {
+		for k := range a.Styles {
+			a.Styles[k] *= (totalSkill * totalCompetency)
+		}
+		armors = append(armors, a)
+	}
+	o.finalArmors = armors
+	o.changedArmors = false
+	return armors, nil
+}
